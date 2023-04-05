f07099079b9f348db61f8bb3f53596826b53c677
HSN <husin@idnplay.com>
2023-04-05 14:50:52 +0700

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: 'IndexPage',
+  // async asyncData({ params, $http }) {
+  //   const post = await axios.get('http://localhost:9090/api/company')
+
+  //   return { post }
+  // }
+}
+</script>
