b7877d1dd85c12cb52a95cf2b9164b57995bed41
HSN <husin@idnplay.com>
2023-04-07 15:50:18 +0700

    up

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/powerhuman-frontend/'
-  },
+  // router: {
+  //   base: '/powerhuman-frontend/'
+  // },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
