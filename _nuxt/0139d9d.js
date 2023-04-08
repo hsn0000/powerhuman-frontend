640c9c800328c43c1ff11ec64f9d542196e701bd
HSN <husin@idnplay.com>
2023-04-08 11:08:20 +0700

    b7877d1

---
+++ _nuxt/0139d9d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{321:function(n,t,e){"use strict";e.r(t);var l=e(21),component=Object(l.a)({},(function(){this._self._c;return this._m(0)}),[function(){var n=this._self._c;return n("div",[n("p",[this._v("Hello Word")])])}],!1,null,null,null);t.default=component.exports}}]);
